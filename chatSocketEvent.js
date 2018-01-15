20f2ea0430a5afd21322f0f470d586003f845a95
pham.viet.toan <FRAMGIA\pham.viet.toan@framgia0238-pc.framgia.com>
2018-01-15 13:32:35 +0700

    Guest user send message and store message

--- chatSocketEvent.js
+++ chatSocketEvent.js
@@ -1,5 +1,44 @@
+var Customer = require('./app/Models/Customer');
+var Message = require('./app/Models/Message');
+
 module.exports = function(io) {
     io.on('connection', function(socket) {
-        console.log(socket.id);
+        socket.on('guest-send-message', function (data) {
+            Customer.findOne({ 'email': data.email }, function(err, customer) {
+                var customer_id = '';
+                if (err) {
+                    console.log(err);
+                    return err;
+                }
+                if (customer) {
+                    customer_id = customer._id;
+                } else {
+                    var newCustomer = new Customer();
+                    newCustomer.email = data.email;
+                    newCustomer.name = data.name;
+                    newCustomer.save(function(err) {
+                        if (err) {
+                            throw err;
+                        }
+
+                        customer_id = newCustomer._id;
+                    });
+                }
+
+                var newMessage = new Message;
+                newMessage.room_id = socket.id;
+                newMessage.sender_id = customer_id;
+                newMessage.receive_id = data.appId;
+                newMessage.type_id = data.type;
+                newMessage.content = data.message;
+                newMessage.save(function(err) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    return newMessage;
+                });
+            });
+        })
     });
 }
