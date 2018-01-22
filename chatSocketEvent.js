577ba7746ebed35584cecb7c5f6b6c12b0052a46
pham.viet.toan <FRAMGIA\pham.viet.toan@framgia0238-pc.framgia.com>
2018-01-22 16:04:59 +0700

    show message box

--- chatSocketEvent.js
+++ chatSocketEvent.js
@@ -1,15 +1,17 @@
 var Customer = require('./app/Models/Customer');
 var Message = require('./app/Models/Message');
+var date = require('date-and-time');
 
 module.exports = function(io) {
     io.on('connection', function(socket) {
-        console.log(socket.id);
         socket.on('agent-online', function (data) {
             socket.join(data);
             socket.agent_id = data;
+            console.log(socket.adapter.rooms);
         });
 
         socket.on('guest-send-message', function (data) {
+            
             Customer.findOne({ 'email': data.email }, function(err, customer) {
                 if (err) {
                     return err;
@@ -17,6 +19,7 @@ module.exports = function(io) {
                 if (!customer) {
                     return false;
                 }
+                let now = new Date();
                 var newMessage = new Message;
                 newMessage.room_id = data.appId;
                 newMessage.user_id = customer._id;
@@ -24,12 +27,20 @@ module.exports = function(io) {
                 newMessage.receive_id = data.appId;
                 newMessage.type_id = data.type;
                 newMessage.content = data.message;
+                newMessage.created_at = date.format(now, 'YYYY/MM/DD HH:mm:ss');
+                newMessage.updated_at = date.format(now, 'YYYY/MM/DD HH:mm:ss');
                 newMessage.save(function(err) {
                     if (err) {
                         throw err;
                     }
-
-                    return newMessage;
+                    let sockets = io.sockets.sockets;
+                    for (i in sockets) {
+                        if (sockets[i].agent_id == data.appId) {
+                            socket.join(data.appId);
+                            io.to(sockets[i].id).emit('server-send-guest-new-message', newMessage);
+                        }
+                    }
+                    return true;
                 });
             });
         });
@@ -41,11 +52,22 @@ module.exports = function(io) {
                 }
                 if (!customer) {
                     var newCustomer = new Customer();
+                    let now = new Date();
                     newCustomer.email = data.email;
                     newCustomer.name = data.name;
+                    newCustomer.created_at = date.format(now, 'YYYY/MM/DD HH:mm:ss');
+                    newCustomer.updated_at = date.format(now, 'YYYY/MM/DD HH:mm:ss');
                     newCustomer.save(function(err) {
                         if (err) {
                             throw err;
+                        }
+                        let sockets = io.sockets.sockets;
+                        for (i in sockets) {
+                            if (sockets[i].agent_id == data.appId) {
+                                console.log(1);
+                                socket.join(data.appId);
+                                io.to(sockets[i].id).emit('server-send-new-guest', newCustomer);
+                            }
                         }
                         return true;
                     });
